6f51f16945dff19c5a0e4b8e1ebcd9a6509aeb97
xavierrocks <xruizify@gmail.com>
2019-02-09 14:20:41 -0500

    Added classes for oop
    👏

---
+++ classes.js
@@ -0,0 +1,65 @@
+class Stadium {
+    constructor(stadiumName){
+        this.stadiumName = stadiumName;
+        this.vendors = [];
+    }
+    constructor(stadiumName, vendors) {
+        this.stadiumName = stadiumName;
+        this.vendors = vendors;
+    }
+    addVendor(vendor) {
+        this.vendors.push(vendor);
+    }
+}
+
+class Customer {
+    constructor(firstName, lastName, phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+    }
+    order(vendor, order) {
+        vendor.addOrder(order);
+    }
+}
+
+class FoodItem {
+    constructor(name, price, amount) {
+        this.name = name;
+        this.price = price;
+        this.amount = amount
+    }
+}
+
+class Order {
+    constructor(customer, foodItems) {
+        this.customer = customer;
+        this.foodItems = foodItems;
+    }
+    addFood(foodItem) {
+        this.foodItems.push(foodItem);
+    }
+}
+
+class Vendor {
+    constructor(vendName){
+        this.vendName = vendName;
+        this.foodItems = [];
+        this.orders = [];
+    }
+    constructor(vendName, foodItems){
+        this.vendName = vendName;
+        this.foodItems = foodItems;
+    }
+    addFood(foodItem) {
+        this.foodItems.push(foodItem);
+    }
+
+    addOrder(order) {
+        this.orders.push(order);
+        // start timer
+        // notify customer through twilio
+        
+    }
+
+}
